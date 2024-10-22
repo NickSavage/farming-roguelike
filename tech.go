e61c9e8e5784d4d1d7c53507e3da2215feaffd4d
Nick Savage <nick@nicksavage.ca>
2024-10-21 20:38:00 -0400

    chopping trees

--- tech.go
+++ tech.go
@@ -263,6 +263,10 @@ func ChopTree(g *Game) {
 
 	scene := g.Scenes["Board"]
 	log.Printf("square %v", scene.Menu.BoardSquare)
+	//	grid := scene.Data["Grid"].([][]BoardSquare)
+	scene.Menu.BoardSquare.Tile = g.Data["GrassTile"].(Tile)
+	scene.Menu.BoardSquare.TileType = "Grass"
+	g.Run.RoundActionsRemaining -= 1
 }
 
 func BlankAction(g *Game) {}
